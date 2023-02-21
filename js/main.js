b54da3ef9c97598ae3e8662dfff428e3e0fdc9d0
Mattia Moneta <moneta.mattia@yahoo.com>
2023-02-21 14:27:11 +0100

    Final

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+let userName = prompt("Inserisci il tuo nome: ");
+let lastName = prompt("Inserisci il tuo cognome: ");
+let favColor = prompt("Qual'è il tuo colore preferito?: ");
+
+document.getElementById("psw-box").innerHTML = userName + lastName + favColor + 23;
